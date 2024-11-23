8297fb305cb139ba5ef2bdd79379d2ba735d3d34
dlgudwn94 <dlgudwn82@naver.com>
2024-11-24 01:56:05 +0900

    mission-2 제출

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,46 @@
 
 */
 
+import { data } from './data.js';
 
+const buttons = document.querySelectorAll('.nav button');
+const liElements = document.querySelectorAll('li');
 
+function setBgcolor(colorA, colorB) {
+  const body = document.body;
+  const setBg = `linear-gradient(to bottom, ${colorA}, ${colorB})`;
 
+  body.style.background = setBg;
+}
+function setImage(name, alt) {
+  const target = document.querySelector('.container .visual div img');
+  target.src = `./assets/${name}.jpeg`;
+  target.alt = alt;
+}
+function setNameText(name) {
+  const target = document.querySelector('.container .nickName');
+  target.textContent = name;
+}
 
+buttons.forEach((button) => {
+  button.addEventListener('click', function () {
+    liElements.forEach((li) => li.classList.remove('is-active'));
+    const li = this.closest('li');
+    li.classList.add('is-active');
 
-
-
-
-
-
-
-
-
+    const index = li.dataset.index - 1;
+    const {
+      color: [colorA, colorB],
+      name,
+      alt,
+    } = data[index];
+    // let colorA, colorB, name, alt;
+    // colorA = data[index].color[0];
+    // colorB = data[index].color[1];
+    // name = data[index].name;
+    // alt = data[index].alt;
+    setBgcolor(colorA, colorB);
+    setImage(name, alt);
+    setNameText(name);
+  });
+});
